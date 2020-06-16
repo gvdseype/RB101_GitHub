334b17f5e057b57410aaa438f7471b5f674f95e7
Gilles Vanderseypen <gillesvanderseypen@gmail.com>
2020-06-16 12:30:18 +0200

    routine update

--- method.rb
+++ method.rb
@@ -1,13 +1,3 @@
+hash = {"invalid" => :invalid}
 
-def nb_year(base, population_increase, new_habitants, end_year)
-  population_increase = population_increase / 100
-  index = 0
-  loop do
-    index += 1
-    base += (base * (population_increase)) + new_habitants
-    break if base >= end_year
-    end
-  p index
-end
-
-nb_year(1500, 5, 100, 5000)
+p hash["invalid"]
--- sides.rb
+++ sides.rb
@@ -1,21 +1,40 @@
-def can_be_integer?(input)
-  (input).to_i.to_s == input
+HASH = {"invalid" => :invalid, "equilateral" => :equilateral, "isosceles" => :isosceles, "scalene" => :scalene}
+
+def made_array!(num1, num2, num3)
+  example_array = []
+  example_array << num1 << num2 << num3
+  example_array
 end
 
-def friend(friends)
-  new_array = []
-  new_array = friends.map do |element|
-    case element
-    when can_be_integer?(element) == true
-      friends.delete(element)
-    when element.split().size > 4
-      friends.delete(element)
-    else 
-      element
+def is_valid_triangle?(num1, num2, num3)
+  array = made_array!(num1, num2, num3).sort { |a, b| a <=> b }
+  if (array[0] + array[1] > array[2]) && (array[0] != 0 && array[1] != 0 && array[2] != 0)
+    true
+  else
+    false
+  end
+end
+
+def delete_duplis!(num1, num2, num3)
+  made_array!(num1, num2, num3).uniq.length #this will return the length of the array after having reduced duplicates to a single occurence
+end
+
+def triangle(num1, num2, num3)
+  if is_valid_triangle?(num1, num2, num3) == false
+    p HASH["invalid"]
+  else
+    if delete_duplis!(num1, num2, num3) == 2 
+      p HASH["isosceles"]
+    elsif delete_duplis!(num1, num2, num3) == 1
+      p HASH["equilateral"] 
+    else
+      p HASH["scalene"]
     end
   end
-  p new_array
 end
 
-
-friend(["Ryan", "Jimmy", "123", "4", "Cool Man"])
+triangle(3, 3, 3) 
+triangle(3, 3, 1.5) 
+triangle(3, 4, 5) 
+triangle(0, 3, 3) 
+triangle(3, 1, 1) 
